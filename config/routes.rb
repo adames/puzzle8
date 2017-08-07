759c317aef4266cad38bfcd30507ea992951cd83
adames <adameshh@gmail.com>
2017-08-07 10:10:36 -0400

    adding route merge

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   resources :games, only: [:show, :index, :create, :update]
   resources :users, only: [:show, :index, :create, :update]
   resources :images, only: [:show, :index]
-<<<<<<< HEAD
-  get '/games/:id/solution', to: 'games#solution'
-=======
 
+  get '/games/:id/solution', to: 'games#solution'
   post '/games/hint', to: 'games#hint'
->>>>>>> 624cc09f2bfd1881c5f6f96ee7e907d26f25afe3
 end
